f767fadfd9616f299c8d19ba8534ac71445bf5b1
R Tsang <richard.tsang9641@gmail.com>
2021-03-16 23:34:03 +0800

    added smooth behavior for scrollTop

--- script/script.js
+++ script/script.js
@@ -22,7 +22,11 @@ var st = document.getElementById("scroll-back-btn");
 st.addEventListener("click", scrollTop);
 
 function scrollTop() {
-  window.scrollTo(0, 0);
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
 }
 
 function requestTick() {
