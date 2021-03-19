a52e8807b46c47510264caafcdf19fd4c62c8ec0
R Tsang <richard.tsang9641@gmail.com>
2021-03-20 00:30:30 +0800

    Updated visual design

--- script/script.js
+++ script/script.js
@@ -1,68 +1,62 @@
-var target = document.getElementsByTagName("div");
-var movers = Object.values(target).filter((t) => t.hasAttribute("data-sad")),
-  lastScrollY = 0,
-  ticking = false;
-window.addEventListener("scroll", onScroll, false);
-
-// export function init() {
-//   var target = document.getElementsByTagName("div");
-//   var tArray = Object.values(target).filter((t) => t.hasAttribute("data-sad"));
-//   tArray.map((t, index) => {
-
-//   });
-// }
-//return array which contains data-sad
-
-function onScroll() {
-  lastScrollY = window.scrollY;
-  requestTick();
-}
-
-var st = document.getElementById("scroll-back-btn");
-st.addEventListener("click", scrollTop);
-
-function scrollTop() {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: 'smooth'
-  });
-}
-
-function requestTick() {
-  if (!ticking) {
-    requestAnimationFrame(update);
-    ticking = true;
-  }
-}
-function update() {
-  var mover = null,
-    moverTop = [],
-    WindowHeight = window.innerHeight *0.6,
-    offset = 0;
-
-  for (var m = 0; m < movers.length; m++) {
-    mover = movers[m];
-    moverTop[m] = mover.offsetTop;
+export const init = () => {
+  const target = document.getElementsByTagName("div");
+  const movers = Object.values(target).filter((t) =>
+    t.hasAttribute("data-sad")
+  );
+  var lastScrollY = window.scrollY,
+    ticking = false;
+  const WindowHeight = window.innerHeight * 0.6;
+  update();
+  function onScroll() {
+    lastScrollY = window.scrollY;
+    requestTick();
   }
 
-  for (var m = 0; m < movers.length; m++) {
-    mover = movers[m];
+  var st = document.getElementById("scroll-back-btn");
+  st.addEventListener("click", scrollTop);
 
-    if (
-      lastScrollY > moverTop[m] - WindowHeight &&
-      mover.getAttribute("data-view") != "true"
-    ) {
-      mover.setAttribute("data-view", "true");
-    } else if (
-      lastScrollY <= moverTop[m] - WindowHeight &&
-      mover.getAttribute("data-view") == "true"
-    ) {
-      mover.setAttribute("data-view", "false");
+  function scrollTop() {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
+
+  function requestTick() {
+    if (!ticking) {
+      requestAnimationFrame(update);
+      ticking = true;
     }
   }
+  function update() {
+    var mover = null,
+      moverTop = [];
 
-  ticking = false;
-}
+    for (var m = 0; m < movers.length; m++) {
+      mover = movers[m];
+      moverTop[m] = mover.offsetTop;
+    }
 
-// init();
+    for (var m = 0; m < movers.length; m++) {
+      mover = movers[m];
+      if (
+        lastScrollY > moverTop[m] - WindowHeight &&
+        mover.getAttribute("data-view") != "true"
+      ) {
+        mover.setAttribute("data-view", "true");
+      } else if (
+        lastScrollY <= moverTop[m] - WindowHeight &&
+        mover.getAttribute("data-view") == "true"
+      ) {
+        mover.setAttribute("data-view", "false");
+      }
+    }
+
+    ticking = false;
+  }
+
+  window.addEventListener("scroll", onScroll, false);
+};
+
+init();
