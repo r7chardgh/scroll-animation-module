5b5681ce0e07d86746b9a7b2f5f988aeea2dd4f1
R Tsang <richard.tsang9641@gmail.com>
2021-05-09 23:19:22 +0800

    modified script

--- src/script/SAD.js
+++ src/script/SAD.js
@@ -28,15 +28,15 @@ export const init =  function () {
           moverTop[m] = mover.offsetTop;
         }
 
-        for (var m = 0; m < movers.length; m++) {
-          mover = movers[m];
+        for (var n = 0; n < movers.length; n++) {
+          mover = movers[n];
           if (
-            lastScrollY > moverTop[m] - WindowHeight &&
+            lastScrollY > moverTop[n] - WindowHeight &&
             mover.getAttribute("data-view") != "true"
           ) {
             mover.setAttribute("data-view", "true");
           } else if (
-            lastScrollY <= moverTop[m] - WindowHeight &&
+            lastScrollY <= moverTop[n] - WindowHeight &&
             mover.getAttribute("data-view") == "true" &&
             mover.getAttribute("data-fill-mode") != "forwards"
           ) {
